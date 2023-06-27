3dfab17dac15088f917bbdca541d413e34b656b3
iMD14 <137671905+iMD14@users.noreply.github.com>
2023-06-27 07:17:48 +0300

    Add files via upload

---
+++ src/Main.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BukkitTeam\BukkitSRV;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+
+class Main extends PluginBase{
+  public function onEnable(): void{
+    $this->getLogger()->info("Enabling the Plugin...");
+    
+    @mkdir($this->getDataFolder());
+    $this->saveResource("servers.yml");
+    #$this->saveDefaultConfig();
+    $this->getResource("servers.yml");
+    $this->servers = new Config($this->getDataFolder() . "servers.yml", Config::YAML);
+    $this->getLogger()->info("Plugin is ready to use");
+  }
+  public function onDisable() : void{
+    $this->getLogger()->info("Disabled");
+  }
+
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+    switch($command->getName()) {
+        case "server":
+            if(count($args) !== 1) {
+                $sender->sendMessage("/server <name>");
+                return false;
+            }
+            $config = new Config($this->getDataFolder() . "servers.yml", Config::YAML);
+            $servers = $config->get("servers");
+
+            $name = $args[0];
+            foreach($servers as $server) {
+                if($server['name'] == $name) {
+                    $sender->sendMessage("Taking you to " . $server['ip'] . ":" . $server['port']);
+                    $sender->sendMessage($server['ip'] . ":" . $server['port']);
+                    if ($sender instanceof Player) {
+                        $sender->transfer($server['ip'], $server['port']);
+                    } else {
+                        $sender->sendMessage("Cannot transfer you to " . $server['name']);
+                    }
+                    return true;
+                }
+            }
+            $sender->sendMessage("Server not found: $name");
+            return false;
+        default:
+            return false;
+    }
+}
+}
