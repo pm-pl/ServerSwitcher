22c350ea044292fe93b25d9825f125dce26b8a7b
NotNellie <134897452+NotNellie@users.noreply.github.com>
2023-06-29 20:59:34 +0300

    Create VersionInfo.php

---
+++ src/VersionInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace iMD14\ServerSwitcher;
+
+final class VersionInfo{
+  public const NAME = "ServerSwitcher";
+  public const VERSION = "1.1";
+  #public const TYPE = "STABLE";
+}
